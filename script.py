52a59f7f7a0373525ca2511d58369f4cbaf8f512
AxFrancois <axel.francois-charlot@cpe.fr>
2022-04-13 17:47:08 +0200

    Jusqu'à Q5

--- script.py
+++ script.py
@@ -7,10 +7,45 @@ github : https://github.com/AxFrancois/Numeric-Comm
 
 """
 
-import dahuffman; scikit-dsp-comm; scikit-commpy
+from dahuffman import HuffmanCodec
+import sk_dsp_comm.fec_block as block
+import matplotlib.pylab as plt
+import math
+#scikit_commpy
 import numpy as np
 
-print("hello world")
+#Q2
+f = open("The_Adventures_of_Sherlock_Holmes_A_Scandal_In_Bohemia.txt", "r")
+txt = f.read()
+char = []
+count = []
 
-a = np.array([1])
-print(a)
+for letter in txt:
+    if letter in char:
+        count[char.index(letter)] += 1
+    else:
+        char.append(letter)
+        count.append(1)
+
+charArray = np.array(char)
+countArray = np.array(count)
+probaArray = countArray/len(txt)
+
+fig = plt.figure()
+ax = fig.add_axes([0.1,0.1,0.85,0.85])
+ax.bar(charArray,probaArray)
+handles, labels = ax.get_legend_handles_labels()
+legend = fig.legend(handles, labels, loc='lower left', ncol=2, frameon=False, bbox_to_anchor=(0.12, 0.88))
+fig.savefig('repartition.jpg', bbox_extra_artists=(ax,legend))
+
+#Q3
+entropy = -np.sum(np.dot(probaArray, np.log2(probaArray)))
+print("Entropy : ", entropy)
+
+#Q4
+huffman_codec_data = HuffmanCodec.from_data(txt)
+#print(huffman_codec_data[:30])
+h_enc_data = huffman_codec_data.encode(txt)
+#print(h_enc_data[:30])
+h_enc_data_str = ''.join(format(byte,'08b') for byte in h_enc_data)
+#print(h_enc_data_str[:30])
